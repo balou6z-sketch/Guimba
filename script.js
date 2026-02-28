d82bddaf1e20daed900c652dc0973325666f1a67
balou6z-sketch <balou6z@icloud.com>
2026-02-28 19:14:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ window.addEventListener('scroll', () => {
         }
     })
 })
+
+const toggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+})
